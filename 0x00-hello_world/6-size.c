b33250e911f820a7c2a5a535de4093b597825b1d
Jesse Iloka <jesseiloka@gmail.com>
2022-09-09 02:21:20 -0700

    Display size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,15 +10,15 @@ int main(void)
 	char a;
 	int b;
 	long int c;
-	long long int d;
+	long  d;
 	float e;
 
-	scanf("%c, %d, %lu, %llu, %f", &a, &b, &c, &d, &e);
+	scanf("%c, %d, %li, %li, %f", &a, &b, &c, &d, &e);
 
-	printf("Size of char: %u byte(s)\n", sizeof(a));
+	printf("Size of char: %lu byte(s)\n", sizeof(a));
 	printf("Size of int: %lu byte(s)\n", sizeof(b));
 	printf("Size of long int %lu byte(s)\n", sizeof(c));
 	printf("Size of long long int %lu byte(s)\n", sizeof(d));
-	printf("Size of float %lu byte(s)\n" sizeof(e));
+	printf("Size of float %lu byte(s)\n", sizeof(e));
 	return (0);
 }
