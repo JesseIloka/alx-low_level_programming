9f05c3e8d087cef6ade3b02350cc2088e788720d
Jesse Iloka <jesseiloka@gmail.com>
2022-09-08 18:51:55 -0700

    Print size of datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char chart
+	int intt
+	long int longintt
+	long long int longlongintt
+	float floatt
+
+	printf("Size of char: %s byte(s)\n", sizeof(chart));
+	printf("Size of int: %d byte(s)\n", sizeof(intt));
+	printf("Size of long int %lld byte(s)\n", sizeof(longintt);
+	printf("Size of long long int %lld byte(s)\n", sizeof(longlongintt));
+	printf("Size of float %f byte(s)\n" sizeof(floatt));
+	return (0);
+
+}
