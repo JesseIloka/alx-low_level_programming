15a00949beb6ab6a3b1adae9e63277e600afb32d
Jesse Iloka <jesseiloka@gmail.com>
2022-09-08 17:49:35 -0700

    Write C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
-puts (const char "Programming is like building a multilingua puzzle" )
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multiingual puzzle.");
+	return(0);
+}
