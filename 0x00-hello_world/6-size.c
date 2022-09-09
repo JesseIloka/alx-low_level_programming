0b0f088f3f2771ba637a44582d3dc0f287c9cd29
Jesse Iloka <jesseiloka@gmail.com>
2022-09-08 19:38:37 -0700

    Generate intel syntax assembly code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,18 @@
  */
 int main(void)
 {
-	char chart;
-	int intt;
-	long int longintt;
-	long long int longlongintt;
-	float floatt;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-	printf("Size of char: %s byte(s)\n", sizeof(chart));
-	printf("Size of int: %d byte(s)\n", sizeof(intt));
-	printf("Size of long int %lld byte(s)\n", sizeof(longintt);
-	printf("Size of long long int %lld byte(s)\n", sizeof(longlongintt));
-	printf("Size of float %f byte(s)\n" sizeof(floatt));
+	scanf("%c, %d, %lu, %llu, %f", &a, &b, &c, &d, &e);
+
+	printf("Size of char: %u byte(s)\n", sizeof(a));
+	printf("Size of int: %lu byte(s)\n", sizeof(b));
+	printf("Size of long int %lu byte(s)\n", sizeof(c));
+	printf("Size of long long int %lu byte(s)\n", sizeof(d));
+	printf("Size of float %lu byte(s)\n" sizeof(e));
 	return (0);
-
 }
