3077a9c5565a869b2a88d185576ba88f8e71c1ac
Jesse Iloka <jesseiloka@gmail.com>
2022-09-09 10:44:05 -0700

    Print albhabets except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,15 @@ int main(void)
 {
 	char alph = 'a';
 
-	while(alph <= 'z')
+	while (alph <= 'z')
 	{
-		if(!(alph == 'q' ||  alph == 'e'))
+		if (!(alph == 'q' ||  alph == 'e'))
 		{
 			putchar(alph);
 		}
 	alph++;
 	}
+	putchar('\n');
 	return (0);
 }
 		
