2802b17254adbb72de0369e067fc4db3ea5fc8a1
Jesse Iloka <jesseiloka@gmail.com>
2022-09-13 17:52:00 +0100

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
+/*
+ * main - print "_putchar with putchar"
+ *
+ * return 0
+ */
 int main(void)
 {
-	write("_putchar");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
 	return (0);
 }
