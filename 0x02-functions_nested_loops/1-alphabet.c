4f594cb457a6551b1f6c1585228d19e60dc941e7
Jesse Iloka <jesseiloka@gmail.com>
2022-09-13 17:15:57 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - print the alphabets
  *
- * Return: Always 0
+ * Return: Always void
  */
 void print_alphabet(void)
 {
@@ -12,9 +12,4 @@ void print_alphabet(void)
 		putchar(letter);
 	putchar('\n');
 	return;
-}
-int main(void)
-{
-	print_alphabet();
-	return (0);
 }
