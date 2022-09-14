7e53f2cf0c5ed9843ba17cacd55787ace23066c9
Jesse Iloka <jesseiloka@gmail.com>
2022-09-14 06:15:50 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of a number
+ * @i: value to compute
+ *
+ * Return: /i/
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		return (-i);
+	else
+		return (i);
+}
