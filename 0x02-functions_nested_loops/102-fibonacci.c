29320447fe611669eeb52db1550398c87535bf98
Jesse Iloka <jesseiloka@gmail.com>
2022-09-14 13:18:15 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print fibonacci sequence
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int count, fib1 = 0, fib2 = 1, sum;
+
+	for (count = 1, count <= 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%d", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf("\n");
+	}
+	return (0);
+}
