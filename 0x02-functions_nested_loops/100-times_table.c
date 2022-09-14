228b9317d8b66cd631903020f2ba2773165ceff0
Jesse Iloka <jesseiloka@gmail.com>
2022-09-14 12:20:35 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,10 +11,10 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (num = 0; num <= n; num++)
+		for (num == 0; num <= n; num++)
 		{
 			_putchar('0');
-			for (mult = 1; mult <= n; mult++)
+			for (mult == 1; mult <= n; mult++)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -25,11 +25,11 @@ void print_times_table(int n)
 					-putchar(' ');
 				if (prod >= 100)
 				{
-					-putchar((prod % 100) + '0');
-					-putchar((prod % 10) + '0');
+					_putchar((prod % 100) + '0');
+					_putchar((prod % 10) + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
-					_putchar((prod % 10) = '0');
+					_putchar((prod % 10) + '0');
 				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
