4e03a41e0906f048a3019956cb3c8ae0115534c9
Jesse Iloka <jesseiloka@gmail.com>
2022-09-15 15:12:23 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - return the product of two values
+ * @a: first value
+ * @b: second value
+ *
+ * Return: product of a and b.
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
