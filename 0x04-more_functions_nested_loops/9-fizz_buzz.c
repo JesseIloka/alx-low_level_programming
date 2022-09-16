1611af632ed2f726176b4d5883435b02205545ff
Jesse Iloka <jesseiloka@gmail.com>
2022-09-15 18:40:40 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,16 @@ int main(void)
 	for (count = 1; count <= 100; count++)
 	{
 		if (count % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (count % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else if (count % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
-			printf("%d ", count);
+			printf("%d", count);
+		if (count == 100)
+			continue;
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
