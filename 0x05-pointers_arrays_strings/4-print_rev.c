c5e7a21fcc94d6a70529ad41efbf2657fc427525
Jesse Iloka <jesseiloka@gmail.com>
2022-09-19 12:48:39 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: the string
+ */
+void print_rev(char *s)
+{
+	char r[];
+
+	while(*s)
+	{
+		r = -*s++;
+	}
+}
