de80b145b852488d155f7948b6fb4fcdc41a213e
Jesse Iloka <jesseiloka@gmail.com>
2022-09-22 01:23:04 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,6 +12,6 @@ char *_strncpy(char *dest, char *src, int n)
 	int index = 0;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[index] = sr[index];
+		dest[index] = src[index];
 	return (dest);
 }
