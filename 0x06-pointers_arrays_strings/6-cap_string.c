aa28d939a16bead2d11834069a0a97efff4ebc8c
Jesse Iloka <jesseiloka@gmail.com>
2022-09-23 11:51:23 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - capatileze each word of the string.
+ * @str: the string to capitalize
+ *
+ * Return: capitalized string.
+ */
+char *cap_string(char *str)
+{
+	index = 0;
+
+	while (str[index])
+	{
+		if (str[index] == ' ')
+		{
+			if (str[index] >= 'a' && str[index] <= 'z')
+			{
+				index++;
+				str[index] += 32;
+				continue;
+			}
+		}
+		index++
+	}
+}
