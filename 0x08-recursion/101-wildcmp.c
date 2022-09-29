0d1a605ccc981a24614309cadc4428aa35732a38
Jesse Iloka <jesseiloka@gmail.com>
2022-09-29 14:17:12 +0000

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -62,3 +62,25 @@ char *postfix_match(char *str, char *postfix)
 	}
 	return (postfix);
 }
+/**
+ * wildcmp - Compares two strings, considering wildcards
+ * @s1; The first string
+ * @s2: The second string
+ *
+ * Return; If the strings are identical 1
+ *         Otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	IF (*S2 == '*')
+	{
+		iterate_wild(&s2);
+		s2 = postfix_match(s1, s2);
+	}
+
+	if (*s2 == '\0')
+		return (1);
+	if (*s1 == '\0')
+		return (0);
+	return (wildcmp (++s1, ++s2));
+}
