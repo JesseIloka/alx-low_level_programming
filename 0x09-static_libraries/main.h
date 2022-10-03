73700cdcdf9abe4b736998249f7cf14dc0d2c7ce
Jesse Iloka <jesseiloka@gmail.com>
2022-10-03 10:05:28 +0000

    libmy.a

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -10,7 +11,7 @@ int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
-int atoi(char *s);
+int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
