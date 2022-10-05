0c3e656112501c291ceeecfb16d9e4960eadb1a6
Jesse Iloka <jesseiloka@gmail.com>
2022-10-05 03:04:13 +0000

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,16 +12,20 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int array2d;
+	int **array2d;
 	int i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	array2d = malloc(sizeof(int) * width * height);
+	array2d = malloc(sizeof(int *) * height);
+	if (array2d == NULL)
+		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
+		array2d[i] = malloc(sizeof(int) * width);
+
 		for (j = 0; j < width; j++)
 			array2d[i][j] = 0;
 	}
