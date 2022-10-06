f6612157816347ad94800d4ea51e3018430a047e
Jesse Iloka <jesseiloka@gmail.com>
2022-10-06 20:54:34 +0000

    malloc_free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,26 +3,43 @@
 
 /**
  * _realloc - reallocates a memory block using malloc and free
- * @prt: a pointer to the memory previously allocated with a call to malloc
+ * @ptr: a pointer to the memory previously allocated with a call to malloc
  * @old_size: the size of the allocated space ptr
  * @new_size: the new size of the new memory block
  *
  * Return: one thing like that
  */
-void *_realloc(void *ptr, unsigned int old_size __attribute__((unused)), unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *mem;
+	char *copy, *fill;
+	unsigned int i;
 
 	if (new_size == old_size)
-		return (*ptr);
+		return (ptr);
 	if (ptr == NULL)
-		return (malloc(new_size));
-	if (new_size == 0 || ptr != NULL)
-		return (free(ptr));
-	if (new_size > old_size)
+	{
+		mem = malloc(new_size);
+		if (mem == NULL)
+			return (NULL);
+		return (mem);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
 		return (NULL);
-	mem = malloc(new_size + old_size);
-	if (mem = NULL)
+	}
+	copy = ptr;
+	mem = malloc(sizeof(*copy) * new_size);
+	if (mem == NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
+	fill = mem;
+	for (i = 0; i < old_size && i < new_size; i++)
+		fill[i] = *copy++;
 	free(ptr);
+	return (mem);
 }
